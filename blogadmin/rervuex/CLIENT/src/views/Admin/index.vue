d6f95afc28b78153527c1a24a14b8b47b60f8907
994AK <1505398065@qq.com>
2020-07-16 06:53:43 +0800

    DOM样式,信息,可视窗,修改

---
+++ blogadmin/rervuex/CLIENT/src/views/Admin/index.vue
@@ -0,0 +1,28 @@
+<template>
+   <div>
+       <el-container>
+           <el-aside width="200">aside</el-aside>
+           <el-main>main</el-main>
+
+       </el-container>
+
+   </div>
+</template>
+
+<script>
+    export default {
+        name: "Admin"
+    }
+</script>
+
+<style scoped lang="less">
+    .el-aside{
+        height: 100vh;
+        background-color: beige;
+    }
+
+    .el-main{
+        height: 100vh;
+
+    }
+</style>
